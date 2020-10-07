1e32d808bc6dcd890ac997989b1885914f751709
Rohit Jose <rohitjose@gmail.com>
2020-10-07 18:33:32 +1100

    Decouples implementation logic

--- gophercises/quiz_game/part1/main.go
+++ gophercises/quiz_game/part1/main.go
@@ -1,14 +1,9 @@
 package main
 
 import (
-	"bufio"
-	"encoding/csv"
 	"flag"
 	"fmt"
-	"io"
-	"log"
 	"os"
-	"strings"
 )
 
 func main() {
@@ -16,40 +11,7 @@ func main() {
 	csvFilePath := flag.String("file", "../problems.csv", "Questions file path")
 	flag.Parse()
 
-	// Read the csv file for the quiz problems
-	// Open the CSV file
-	problemsFile, err := os.Open(*csvFilePath)
-	if err != nil {
-		log.Fatalln("Could not open the CSV file", err)
-	}
-
-	// Parse the file
-	r := csv.NewReader(problemsFile)
-	rans := 0
-
-	// Iterate through the records
-	for {
-		// Read each record from csv file
-		q, err := r.Read()
-		if err == io.EOF {
-			break
-		}
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		// Read Input
-		fmt.Printf("What is %s ? ", q[0])
-		reader := bufio.NewReader(os.Stdin)
-		ans, _ := reader.ReadString('\n')
-		// Trim the endline character
-		cans := strings.Trim(ans, " \n")
-
-		if cans == q[1] {
-			rans++
-		}
-	}
+	rans := runQuiz(os.Stdin, *csvFilePath)
 
 	// Results
 	fmt.Printf("You got %d answers right!!!", rans)
---
+++ gophercises/quiz_game/part1/quiz.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+func runQuiz(stdin io.Reader, path string) int {
+	// Read the csv file for the quiz problems
+	// Open the CSV file
+	problemsFile, err := os.Open(path)
+	if err != nil {
+		log.Fatalln("Could not open the CSV file", err)
+	}
+
+	// Parse the file
+	r := csv.NewReader(problemsFile)
+	rans := 0
+
+	// Iterate through the records
+	for {
+		// Read each record from csv file
+		q, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Read Input
+		fmt.Printf("What is %s ? ", q[0])
+		reader := bufio.NewReader(stdin)
+		ans, _ := reader.ReadString('\n')
+		// Trim the endline character
+		cans := strings.Trim(ans, " \n")
+
+		if cans == q[1] {
+			rans++
+		}
+	}
+
+	return rans
+}

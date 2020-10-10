4aa8068545b92b414116b6c6d98af09354619233
Rohit Jose <rohitjose@gmail.com>
2020-10-10 12:24:31 +1100

    Tabled tests

--- gophercises/quiz_game/part1/quiz_test.go
+++ gophercises/quiz_game/part1/quiz_test.go
@@ -6,13 +6,34 @@ import (
 )
 
 func TestRunQuiz(t *testing.T) {
-	// Initialise the user input
-	var stdin bytes.Buffer
-	stdin.Write([]byte("10\n2\n11\n3\n14\n4\n5\n6\n5\n6\n6\n7\n"))
-
 	csvFilePath := "../problems.csv"
-	result := runQuiz(&stdin, csvFilePath)
-	if result != 12 {
-		t.Errorf("Want 12 Got %d", result)
+	tests := []struct {
+		name  string
+		input []byte
+		want  int
+	}{
+		{
+			name:  "all_valid",
+			input: []byte("10\n2\n11\n3\n14\n4\n5\n6\n5\n6\n6\n7\n"),
+			want:  12,
+		},
+		{
+			name:  "one_invalid",
+			input: []byte("9\n2\n11\n3\n14\n4\n5\n6\n5\n6\n6\n7\n"),
+			want:  11,
+		},
+		{
+			name:  "all_invalid",
+			input: []byte("9\n1\n1\n1\n1\n1\n1\n1\n1\n1\n1\n1\n"),
+			want:  0,
+		},
+	}
+	for _, tc := range tests {
+		var stdin bytes.Buffer
+		stdin.Write(tc.input)
+		result := runQuiz(&stdin, csvFilePath)
+		if result != tc.want {
+			t.Errorf("Want %d Got %d", tc.want, result)
+		}
 	}
 }
